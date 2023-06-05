ebc5f1bdc146a9d34702f508a1c39ee4a86448ec
Chris Johnson <gordug@outlook.com>
2023-06-05 09:41:23 +0100

    Fixed Copy Button
    Applied Bootstrap styling to form elements
    
    Created Common Service Folder

---
+++ Site/Data/Common/ClipboardService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace gordug.uk.Data.Common;
+
+public interface IClipboardService
+{
+    ValueTask WriteTextAsync(string text);
+}
+
+internal class ClipboardService : IClipboardService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public ClipboardService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public ValueTask WriteTextAsync(string text)
+    {
+        return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
